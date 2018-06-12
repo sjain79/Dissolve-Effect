9dcbaf999013837b7efe9f42e9476bfbc79eb672
Unknown <sjain@student.fullsail.edu>
2018-06-12 02:53:49 -0400

    Whole Project

---
+++ Dissolve Effect/Assets/Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime*speed);
+		
+	}
+}
