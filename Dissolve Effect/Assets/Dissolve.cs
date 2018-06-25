e7cd3ccedbf0259a8789984338bdca088ae7cf21
Unknown <sjain@student.fullsail.edu>
2018-06-25 11:15:55 -0400

    Completed the dissolve effect

---
+++ Dissolve Effect/Assets/Dissolve.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour {
+
+	void Start ()
+    {
+        //dissolve = Shader.Find("graphs/Dissolve");
+	}
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<SkinnedMeshRenderer>().material.SetFloat("Vector1_950447AF", Mathf.Lerp(GetComponent<SkinnedMeshRenderer>().material.GetFloat("Vector1_950447AF"), 1.0f, 0.025f));
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            GetComponent<SkinnedMeshRenderer>().material.SetFloat("Vector1_950447AF", Mathf.Lerp(GetComponent<SkinnedMeshRenderer>().material.GetFloat("Vector1_950447AF"), 0, 0.025f));
+        }
+    }
+}
